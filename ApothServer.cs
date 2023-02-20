c1f0405d49552000971aa9e6d14b2b0bb9b92410
vxrynir <74647886+vxrynir@users.noreply.github.com>
2023-02-20 14:00:17 -0500

    cleaned up server class and made working startup

---
+++ ApothServer.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Apotheosis
+{
+    public class ApothServer
+    {
+        int listeningPort = 0;
+        UdpClient client;
+
+        public ApothServer(int port)
+        {
+            listeningPort = port;
+            client = new UdpClient(listeningPort);
+        }
+
+        public async Task Start()
+        {
+            while (true)
+            {
+                var data = await client.ReceiveAsync();
+                OnUdpData(data);
+            }
+        }
+
+        void OnUdpData(UdpReceiveResult result)
+        {
+            //this is where we handle incoming data from any connection
+            if (result.Buffer != null)
+            {
+                Console.WriteLine($"|{DateTime.Now.ToShortTimeString()}|{result.RemoteEndPoint}:{Encoding.UTF8.GetString(result.Buffer)}");
+
+            }
+            else
+            {
+                //data was null
+            }
+
+
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Apotheosis;
+using System;
+ApothServer server = new ApothServer(666);
+await server.Start();
+
